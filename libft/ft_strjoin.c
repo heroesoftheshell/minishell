077a764bb56f9140744e2769144858b96e1b6609
Geunyeong Hong <geunyeonghong@gmail.com>
2021-09-01 17:23:24 +0900

    modify odd file author and remove unused code

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: heom <heom@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: ghong <ghong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/04 22:25:07 by hekang            #+#    #+#             */
-/*   Updated: 2021/08/31 12:17:26 by heom             ###   ########.fr       */
+/*   Updated: 2021/09/01 17:20:44 by ghong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,38 +28,6 @@ char	*ft_strjoin_free_a(char *s1, char const *s2)
 	ft_memcpy(str, s1, len1);
 	ft_memcpy(str + len1, s2, len2 + 1);
 	free(s1);
-	return (str);
-}
-
-static void	handle_free(char *s1, char *s2, int free_mode)
-{
-	if (free_mode == FREE_ALL)
-	{
-		free(s1);
-		free(s2);
-	}
-	else if (free_mode == FREE_FIRST_ARG)
-		free(s1);
-	else if (free_mode == FREE_SECOND_ARG)
-		free(s2);
-}
-
-char	*ft_strjoin_with_free(char *s1, char *s2, int free_mode)
-{
-	int		len1;
-	int		len2;
-	char	*str;
-
-	if (s1 == 0 || s2 == 0)
-		return (0);
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
-	str = ft_calloc(len1 + len2 + 1, sizeof(char));
-	if (!str)
-		return (0);
-	ft_memcpy(str, s1, len1);
-	ft_memcpy(str + len1, s2, len2 + 1);
-	handle_free(s1, s2, free_mode);
 	return (str);
 }
 
