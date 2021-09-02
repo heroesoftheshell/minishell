4d302b1611f6defd71989db5ba0b4c3081a409c3
hekang <glipung@naver.com>
2021-09-02 13:44:48 +0900

    modify signal

--- srcs/init_signal.c
+++ srcs/init_signal.c
@@ -6,7 +6,7 @@
 /*   By: hekang <hekang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 09:30:46 by hekang            #+#    #+#             */
-/*   Updated: 2021/08/31 17:15:29 by hekang           ###   ########.fr       */
+/*   Updated: 2021/09/02 11:46:13 by hekang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,4 +40,22 @@ void	init_signal(void)
 	rl_catch_signals = 0;
 	signal(SIGINT, catch_function);
 	signal(SIGQUIT, do_nothing);
+}
+
+void	handle_exit_status(int exit_status)
+{
+	int	sig_num;
+
+	sig_num = 0;
+	if (WTERMSIG(exit_status) == SIGINT || WTERMSIG(exit_status) == SIGQUIT)
+		sig_num = WTERMSIG(exit_status);
+	if (sig_num == SIGINT)
+		all()->end_code = 130;
+	else if (sig_num == SIGQUIT)
+	{
+		write(1, "QUIT: 3\n", 8);
+		all()->end_code = 131;
+	}
+	if (!WTERMSIG(exit_status))
+		all()->end_code = WEXITSTATUS(exit_status);
 }
