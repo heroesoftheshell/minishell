896c97a74c9ec5767c609d411a07c8e82c8b43a4
hekang <glipung@naver.com>
2021-09-02 16:21:15 +0900

    modify validate_env_key after evaluation

--- srcs/validate_env_key.c
+++ srcs/validate_env_key.c
@@ -6,17 +6,20 @@
 /*   By: hekang <hekang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/20 12:19:44 by hekang            #+#    #+#             */
-/*   Updated: 2021/08/30 16:35:44 by hekang           ###   ########.fr       */
+/*   Updated: 2021/09/02 16:10:23 by hekang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	validate_env_key(char *key, int unset_key)
+int	validate_env_key(char *cmd, int unset_key)
 {
-	int	cnt;
+	int		cnt;
+	char	*key;
+	char	*value;
 
 	cnt = 0;
+	get_key_value(cmd, &key, &value);
 	if (ft_isalpha(key[0]) || key[0] == '_')
 	{
 		while (key[++cnt])
